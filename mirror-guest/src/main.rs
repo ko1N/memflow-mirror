7207dafa2ab6ce4b66063802d326909d8b8794f4
ko1N <ko1N1337@gmail.com>
2021-04-01 12:49:25 +0100

    Improved frame counting

--- mirror-guest/src/main.rs
+++ mirror-guest/src/main.rs
@@ -35,62 +35,50 @@ fn main() {
     let start = Instant::now();
     let mut frame_counter = 0u32;
     loop {
-        // wait for frame to be read
-        loop {
-            unsafe {
-                if let Some(global_buffer) = &GLOBAL_BUFFER {
-                    let frame_read_counter =
-                        std::ptr::read_volatile(&global_buffer.frame_read_counter);
-                    if frame_read_counter == global_buffer.frame_counter {
-                        break;
-                    }
-                }
+        // check if the frame has been read and we need to generate a new one
+        let update_frame = unsafe {
+            if let Some(global_buffer) = &GLOBAL_BUFFER {
+                let frame_read_counter =
+                    std::ptr::read_volatile(&global_buffer.frame_read_counter);
+                frame_read_counter == global_buffer.frame_counter
+            } else {
+                false
             }
-        }
+        };
 
         // update frame
-        if let Ok(frame) = dxgi.capture_frame() {
-            // frame captured, put into global buffer
-            unsafe {
-                if let Some(global_buffer) = &mut GLOBAL_BUFFER {
-                    // TODO: doesnt work for all resolutions?
-                    if global_buffer.frame_buffer.len() != frame.0.len() * 4 {
-                        println!("changing resolution: {:?}", frame.1);
-                        global_buffer.width = frame.1 .0;
-                        global_buffer.height = frame.1 .1;
-                        global_buffer.frame_buffer = vec![0u8; frame.0.len() * 4];
+        if update_frame {
+            if let Ok(frame) = dxgi.capture_frame() {
+                // frame captured, put into global buffer
+                frame_counter += 1;
+
+                unsafe {
+                    if let Some(global_buffer) = &mut GLOBAL_BUFFER {
+                        // TODO: doesnt work for all resolutions?
+                        if global_buffer.frame_buffer.len() != frame.0.len() * 4 {
+                            println!("changing resolution: {:?}", frame.1);
+                            global_buffer.width = frame.1 .0;
+                            global_buffer.height = frame.1 .1;
+                            global_buffer.frame_buffer = vec![0u8; frame.0.len() * 4];
+                        }
+                        global_buffer
+                            .frame_buffer
+                            .copy_from_slice(slice::from_raw_parts(
+                                frame.0.as_ptr() as *const u8,
+                                frame.0.len() * 4,
+                            ));
+                        global_buffer.frame_counter = frame_counter;
                     }
-                    global_buffer
-                        .frame_buffer
-                        .copy_from_slice(slice::from_raw_parts(
-                            frame.0.as_ptr() as *const u8,
-                            frame.0.len() * 4,
-                        ));
                 }
             }
         }
 
-        // update cursor
+        // update cursor regardless of the frame_buffer state
         if let Ok(cursor) = cursor::get_state() {
             unsafe {
                 if let Some(global_frame) = &mut GLOBAL_BUFFER {
                     global_frame.cursor = cursor;
                 }
-            }
-        }
-
-        // update frame counter
-        frame_counter += 1;
-        unsafe {
-            if let Some(global_buffer) = &mut GLOBAL_BUFFER {
-                global_buffer.frame_counter = frame_counter;
-            }
-        }
-
-        if (frame_counter % 1000) == 0 {
-            let elapsed = start.elapsed().as_millis() as f64;
-            if elapsed > 0.0 {
-                println!("{} fps", (f64::from(frame_counter)) / elapsed * 1000.0);
             }
         }
     }
